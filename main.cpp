ef986c154f406229fc5e7035b695e23e3b00a17b
S.H.W <2536076378@qq.com>
2024-03-20 17:40:26 +0800

    optimize code

--- main.cpp
+++ main.cpp
@@ -9,10 +9,9 @@ const char* output_path = "out/output.tga";
 static void point(const int x, const int y, TGAImage& image, const TGAColor color)
 {
     image.set(x, y, color);
-    image.flip_vertically();
 }
 
-static void line(const int x0, const int y0, const int x1, const int y1, TGAImage& image, const TGAColor color, const int resulotion = 20)
+static void line(const int x0, const int y0, const int x1, const int y1, TGAImage& image, const TGAColor color, const int resulotion = 100)
 {
     const float step = 1. / (float)resulotion;
 
@@ -27,12 +26,14 @@ static void line(const int x0, const int y0, const int x1, const int y1, TGAImag
 int main()
 {
     printf("begin...\n");
-
     TGAImage image(100, 100, TGAImage::RGB);
 
-    line(10, 10, 60, 60, image, red);
-    image.write_tga_file(output_path);
+    line(13, 20, 80, 40, image, white);
+    line(20, 13, 40, 80, image, red);
+    line(80, 40, 13, 20, image, red);
 
+    image.flip_vertically();
+    image.write_tga_file(output_path);
     printf("ok.");
     return 0;
 }
