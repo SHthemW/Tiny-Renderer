e8f64fbe238e5634e988460fb88e910a6c5af6e9
S.H.W <2536076378@qq.com>
2024-03-26 16:10:28 +0800

    dev: 更连续的线段绘制器

--- graph_drawer.h
+++ graph_drawer.h
@@ -2,21 +2,26 @@
 #include "tgaimage.h"
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
-const TGAColor red = TGAColor(255, 0, 0, 255);
+const TGAColor green = TGAColor(0  , 255,   0, 255);
+const TGAColor red   = TGAColor(255,   0,   0, 255);
 
 static void point(const int x, const int y, TGAImage& image, const TGAColor color)
 {
     image.set(x, y, color);
 }
 
-static void line(const int x0, const int y0, const int x1, const int y1, TGAImage& image, const TGAColor color, const int resulotion = 100)
+static void line(const int x0, const int y0, const int x1, const int y1, TGAImage& image, const TGAColor color = white, const int resulotion = 10)
 {
-    const float step = 1. / (float)resulotion;
+    const int diff_x = x1 - x0;
+    const int diff_y = y1 - y0;
+    const int length = std::sqrt((float)std::abs(diff_x) + (float)std::abs(diff_y));
+
+    const float step = 1. / ((float)resulotion * (float)length);
 
     for (float t = 0.; t < 1; t += step)
     {
-        float x = x0 + t * (x1 - x0);
-        float y = y0 + t * (y1 - y0);
+        float x = x0 + t * diff_x;
+        float y = y0 + t * diff_y;
         image.set(x, y, color);
     }
 }
